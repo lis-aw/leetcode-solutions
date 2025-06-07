ee7459cd95b8e03acae772271ffb023b391b6d9f
Lisa Michèle Schilling <98551187+lis-aw@users.noreply.github.com>
2025-06-07 21:57:02 +0200

    Time: 43 ms (45.65%) | Memory: 18.6 MB (40.79%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -6,33 +6,17 @@ class Node:
         self.next = next
         self.random = random
 """
-
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if head is None: return None
-
-        hashMap={} # will be filled like this: {'originalNode':'Node(originalNode.val) aka newNode'}
-
-        #this loop creates a dict with key:value pairs that are "originalNode:copiedNode"
+        hashMap={}
         originalNode = head
         while originalNode is not None:
-            hashMap[originalNode] = Node(originalNode.val) #new key:value pair with originalNode:copiedNode (<-just has value, not next or random)
-            originalNode = originalNode.next #go to the next Node
-
-        ##and now this loop creates the deepcopy "copy" by calling the dict with the original Node intsances, but since a the dict then returns the value of this key, which is the copied node, we get "copy" which consist of copied nodes and their .next and .random functions also point to copied nodes
+            hashMap[originalNode] = Node(originalNode.val) 
+            originalNode = originalNode.next 
         originalNode = head
         while originalNode is not None:
-            (hashMap[originalNode]).next = hashMap[originalNode.next] if originalNode.next else None #copy.next is now being set as 
+            (hashMap[originalNode]).next = hashMap[originalNode.next] if originalNode.next else None 
             (hashMap[originalNode]).random = hashMap[originalNode.random] if originalNode.random else None
-
             originalNode = originalNode.next
-
-        return hashMap[head]
-
-
-        
-
-
-
-        
-        
+        return hashMap[head]     
