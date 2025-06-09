cd0a5e6668ffc6c06f69a2f741e00f21f20eb331
Lisa Michèle Schilling <98551187+lis-aw@users.noreply.github.com>
2025-06-09 16:55:58 +0200

    Time: 0 ms (100.00%) | Memory: 18.9 MB (33.98%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,12 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        myDict={}
-        idx=0
-        for x in nums:
-            if (target-x) in myDict:
-                return [myDict[target-x],idx]
-            myDict[x]=idx
-            idx+=1
+        mapNumsIndex={}
+        currentIndex=0
+        for currentValue in nums:
+            complement = target-currentValue
+            if (complement) in mapNumsIndex:
+                complementIndex = mapNumsIndex[complement]
+                return [complementIndex,currentIndex] #returns two indices, the one of the complement, and the current one 
+            mapNumsIndex[currentValue]=currentIndex #dict that saves an item of nums (key) with its index(value)
+            currentIndex+=1 #increment the index
 
 
 
