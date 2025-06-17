fd129dbbce942cd17b920286d4fdf5c6e6f873b0
Lisa Michèle Schilling <98551187+lis-aw@users.noreply.github.com>
2025-06-17 15:11:39 +0200

    Time: 45 ms (6.38%) | Memory: 31.2 MB (5.50%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -2,12 +2,9 @@ from collections import defaultdict
 from collections import Counter
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        allSets=[]
         collectAnagrams=defaultdict(list)
-        idx=0
         for val in strs:
-            x=Counter(val)
-            y=frozenset(x.items())
+            y=frozenset(Counter(val).items())
             collectAnagrams[y].append(val)
 
         return(list(collectAnagrams.values()))
