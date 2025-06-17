a0a028e00b24b74a3ba6dfa25c22baffed911995
Lisa Michèle Schilling <98551187+lis-aw@users.noreply.github.com>
2025-06-17 15:47:50 +0200

    Time: 11 ms (83.41%) | Memory: 22.7 MB (13.17%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -4,8 +4,10 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         collectAnagrams = defaultdict(list)
         for val in strs:
-            key = ''.join(sorted(val))  # O(K log K)
-            collectAnagrams[key].append(val)
+            count = [0] * 26
+            for c in val:
+                count[ord(c) - ord("a")] += 1  
+            collectAnagrams[tuple(count)].append(val)
         return list(collectAnagrams.values())
 
 
