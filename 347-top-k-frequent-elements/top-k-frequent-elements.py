f7e80b7389de9141c14072dc9a160591c2b3a843
Lisa Michèle Schilling <98551187+lis-aw@users.noreply.github.com>
2025-06-18 11:41:05 +0200

    Time: 3 ms (90.98%) | Memory: 21.3 MB (40.48%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -3,10 +3,8 @@ import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         countedNums = Counter(nums)
-        x = sorted(countedNums.items(), key=lambda x: -x[1])
-        result=[]
-        for i in range(k):
-            result.append(x[i][0])
+        mostCommon = countedNums.most_common(k)
+        result = [num for num,count in mostCommon]
         return result
         
         
