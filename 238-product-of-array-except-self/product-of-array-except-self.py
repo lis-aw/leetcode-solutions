0a20723a69e9087c76a4c1c7da90989ea38e105a
Lisa Michèle Schilling <98551187+lis-aw@users.noreply.github.com>
2025-06-18 19:02:46 +0200

    Time: 54 ms (6.24%) | Memory: 23.5 MB (41.80%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -0,0 +1,23 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        length = len(nums)
+        output = [1] * length #the first entry is always 1. Also needed for *= operation
+        prefix=nums[0] #first prefix is first number in nums
+        postfix=nums[-1] #first postfix is last number in nums
+
+        i=1#starting at the second entry
+        while i < length:
+            output[i]*=prefix
+            prefix*=nums[i]
+            i+=1
+        print(f"Prefix-Output: {output}")
+
+        i=2#starting at the second to last entry
+        while i <= length:
+          output[-i]*=postfix
+          postfix*=nums[-i]  
+          i+=1
+        print(f"Postfix-Output: {output}")
+        return output
+        
+        
