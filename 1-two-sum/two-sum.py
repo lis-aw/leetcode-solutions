60242f78b3f16f7635cfbbee252011b116c7d382
Lisa Michèle Schilling <98551187+lis-aw@users.noreply.github.com>
2025-06-19 14:45:16 +0200

    Time: 0 ms (100.00%) | Memory: 19.1 MB (10.73%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,16 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mapNumsIndex={}
-        currentIndex=0
-        for currentValue in nums:
-            complement = target-currentValue
-            if (complement) in mapNumsIndex:
-                complementIndex = mapNumsIndex[complement]
-                return [complementIndex,currentIndex] #returns two indices, the one of the complement, and the current one 
-            mapNumsIndex[currentValue]=currentIndex #dict that saves an item of nums (key) with its index(value)
-            currentIndex+=1 #increment the index
-
-
-
-
+        complement = target
+        complementMap = {}
+        result=[]
+        for idx, num in enumerate(nums):
+            complement = target-num #9-2=7
+            if complement in complementMap.keys():#if 7 is already a key in complementMap
+                idxnumber1 = idx #first number is the number at index 0 in nums array
+                idxnumber2 = complementMap[complement] #this returns the index of the complement
+                result = [idxnumber1,idxnumber2]
+                return result    
+            else:
+                complementMap[num] = idx #complementMap{2:0} store number and index of number inside nums array in this dict
         
